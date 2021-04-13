e336a9a9ec6fb6e3a14f16c6ef0564568793f674
Matheus dos Santos Bonifácio <caneladeouro10@gmail.com>
2021-04-13 20:54:53 -0300

    feat (model): adding the validator data

--- server/src/Controllers/ProjectController.php
+++ server/src/Controllers/ProjectController.php
@@ -4,8 +4,10 @@ namespace BShare\Webservice\Controllers;
 
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use Rakit\Validation\Validator;
 
 use BShare\Webservice\Models\Projects;
+use BShare\Webservice\Error\ValidateException;
 
 class ProjectController
 {
@@ -14,8 +16,24 @@ class ProjectController
         // Init variables
         $project = new Projects();
         $uploaddir = $_SERVER['DOCUMENT_ROOT'] . '/assets/project/';
+        $validator = new Validator;
         $uploadfile = $uploaddir . basename($_FILES['project']['name']);
         $data = $_POST + $_FILES;
+
+        $validation = $validator->validate($data, [
+            "title" => "required",
+            "price" => "required|numeric",
+            "ageClassification" => "required|numeric",
+            "category" => "required",
+            "author" => "required|digits:36",
+            "project" => "required|uploaded_file:0,2G,blend",
+            "mainImage" => "uploaded_file:0,2G,png,jpeg",
+            "image" => "uploaded_file:0,2G,png,jpeg",
+        ]);
+
+        if ($validation->fails()) {
+            throw new ValidateException(($validation->errors())->firstOfAll());
+        }
 
         // Getting request body and move to the project object
         $data = $project->registerProject((object) $data);
