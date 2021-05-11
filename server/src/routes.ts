b40bbd582fd3e8f7ab234747ce764e4f04cc7549
Matheus dos Santos Bonifácio <caneladeouro10@gmail.com>
2021-05-11 10:22:55 -0300

    feat (model): adding create folder route

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,13 +1,19 @@
 import { Router } from "express";
+import { FolderController } from "./controllers/FolderController";
 
 import UserController from "./controllers/UserController";
 
 const router = Router();
 const userController = new UserController();
+const folderController = new FolderController();
 
+// User
 router.get("/users", userController.showAll);
 router.get("/user/:id", userController.show);
 router.post("/user/log-in", userController.login);
 router.post("/user", userController.create);
+
+// Folder
+router.post("/folder", folderController.create);
 
 export default router;
