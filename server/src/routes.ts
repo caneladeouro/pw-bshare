c473008cffe6248f8d3f855fcac5374bef4be16f
Matheus dos Santos Bonifácio <caneladeouro10@gmail.com>
2021-05-11 09:10:40 -0300

    feat (model): adding user login route

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -7,6 +7,7 @@ const userController = new UserController();
 
 router.get("/users", userController.showAll);
 router.get("/user/:id", userController.show);
+router.post("/user/log-in", userController.login);
 router.post("/user", userController.create);
 
 export default router;
