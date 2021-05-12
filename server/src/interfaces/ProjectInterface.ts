7f2fbb659a39301486f9a7ae695d0ac453c6fe13
Matheus dos Santos Bonifácio <caneladeouro10@gmail.com>
2021-05-12 01:35:07 -0300

    feat (model): adding project interface

--- server/src/interfaces/ProjectInterface.ts
+++ server/src/interfaces/ProjectInterface.ts
@@ -1,14 +1,11 @@
 interface IProject {
-  id: string;
+  id?: string;
   title: string;
   description: string;
-  age_classification: string;
   price: string;
-  category: ICategory;
-  project: string;
   author: string;
-  comment: string;
-  images: string[];
+  comment?: string;
+  // images: string[];
   main_image: string;
   created_at: string;
 }
