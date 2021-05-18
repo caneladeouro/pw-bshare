16ab06fc807ed0f7e3e2da4bb6ce5c3703343b1f
Matheus dos Santos Bonifácio <caneladeouro10@gmail.com>
2021-05-18 15:22:15 -0300

    feat (model): show image column with project

--- server/src/services/ProjectService.ts
+++ server/src/services/ProjectService.ts
@@ -9,28 +9,46 @@ class ProjectService {
     this.projectRepository = getCustomRepository(ProjectRepository);
   }
 
-  async create({
-    title,
-    description,
-    price,
-    author,
-    comment,
-    main_image,
-    created_at,
-  }: IProject) {
-    const projectAlreadyExist = this.projectRepository.findOne({
-      title,
+  async create(data: IProject) {
+    const projectAlreadyExist = await this.projectRepository.findOne({
+      where: { title: data.title },
     });
 
     if (projectAlreadyExist) {
-      return;
+      return projectAlreadyExist;
     }
+
+    const project = this.projectRepository.create({
+      title: data.title,
+      description: data.description,
+      main_image: data.main_image,
+      category: data.category,
+      price: data.price,
+      project: data.project,
+      images: data.images,
+      active: true,
+    });
+
+    await this.projectRepository.save(project);
+    return project;
   }
 
   async showAll() {
-    const projects = await this.projectRepository.find();
+    const projects = await this.projectRepository.find({
+      relations: ["images"],
+    });
 
-    return projects;
+    return projects.map((project) => {
+      return {
+        ...project,
+        images: project.images.map((image) => {
+          return {
+            ...image,
+            image: `http://127.0.0.1:3100/uploads/${image.image}`,
+          };
+        }),
+      };
+    });
   }
 }
 
