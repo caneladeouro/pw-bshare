7bc7b3985bd70ba5be5e0ae604ccdd09ca81ff4e
bruno-p-reis <brunoreispalermo@gmail.com>
2021-05-22 15:55:25 -0300

    22052021_1555

---
+++ frontend/resources/views/cadastra-pasta.blade.php
@@ -0,0 +1,28 @@
+@extends('layout')
+
+@section('css')
+
+<!--Stylesheets: 1-Bootstrap, 2-Própria-->
+<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+
+<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;1,100;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
+
+@stop
+
+@section('title')
+Criar pasta | BShare
+@stop
+
+@section('logo')
+@stop
+
+@section('content')
+
+<section>
+    <div class="criarPastaContainer">
+        
+    </div>
+</section>
+
+@stop
