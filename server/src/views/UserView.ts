b6809f70ba0494578fd8eebe6c8dc55a8125555a
Matheus dos Santos Bonifácio <caneladeouro10@gmail.com>
2021-06-18 13:32:18 -0300

    feat (views): adding project fields in user view

--- server/src/views/UserView.ts
+++ server/src/views/UserView.ts
@@ -1,16 +1,24 @@
 import { User } from "../entities/User";
+import projectView from "./ProjectView";
 
 export default {
-  render(user: User) {
+  render(user: User, showProjects = false) {
+    const projects = showProjects
+      ? projectView.renderUserMany(user.projects, user.id)
+      : undefined;
+
     return {
       id: user.id,
       front_cover: user.front_cover,
       username: user.username,
       email: user.email,
+      description: user.description,
+      projects: showProjects ? projects.all_projects : undefined,
+      created_projects: showProjects ? projects.created_projects : undefined,
     };
   },
 
-  renderMany(users: User[]) {
-    return users.map((user) => this.render(user));
+  renderMany(users: User[], showProjects = false) {
+    return users.map((user) => this.render(user, showProjects));
   },
 };
