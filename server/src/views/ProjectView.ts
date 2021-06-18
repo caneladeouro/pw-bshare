b3eb2dd7d06defeb7d583c2f1d93a00dccfe271e
Matheus dos Santos Bonifácio <caneladeouro10@gmail.com>
2021-06-18 13:33:15 -0300

    feat (views): adding news fields support in project view

--- server/src/views/ProjectView.ts
+++ server/src/views/ProjectView.ts
@@ -8,7 +8,11 @@ export default {
       id: project.id,
       title: project.title,
       description: project.description,
-      main_image: `http://${process.env.HOST}/uploads/${project.main_image}`,
+      blender_version: project.blender_version,
+      render_engine: project.render_engine,
+      main_image: project.main_image
+        ? `http://${process.env.HOST}/uploads/${project.main_image}`
+        : undefined,
       price: project.price,
       project: `http://${process.env.HOST}/uploads/${project.project}`,
       active: project.active,
@@ -21,5 +25,21 @@ export default {
 
   renderMany(projects: Project[]) {
     return projects.map((project) => this.render(project));
+  },
+
+  renderUserMany(projects: Project[], author_id: string) {
+    var created_projects = [];
+    var all_projects = [];
+
+    projects.map((project) => {
+      let aux_project = this.render(project);
+
+      if (project.author_id == author_id) {
+        created_projects.push(aux_project);
+      }
+      all_projects.push(aux_project);
+    });
+
+    return { created_projects, all_projects };
   },
 };
